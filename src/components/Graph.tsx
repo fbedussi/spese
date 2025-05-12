6259ed18c8c7ef6b81904f0c028525ad329844dc
Francesco Bedussi <francesco@francescobedussi.it>
2025-05-12 10:20:55 +0200

    refactor: html legend

--- src/components/Graph.tsx
+++ src/components/Graph.tsx
@@ -3,6 +3,7 @@ import {
   ArcElement,
   CategoryScale,
   Chart,
+  type ChartType,
   Colors,
   DoughnutController,
   Filler,
@@ -16,9 +17,18 @@ import { format } from 'date-fns';
 import { createEffect } from 'solid-js';
 import { getFilteredDataTotal } from '~/data';
 import { formatMoney } from '~/helpers';
-import { Expense, type SearchParams } from '~/types';
-import { filteredData, filteredDataByCategory } from '../data';
+import type { SearchParams } from '~/types';
+import { filteredDataByCategory } from '../data';
 import styles from './graph.module.css';
+
+// Extend Chart.js types to include the custom htmlLegend plugin
+declare module 'chart.js' {
+  interface PluginOptionsByType<TType extends ChartType> {
+    htmlLegend?: {
+      containerID: string;
+    };
+  }
+}
 
 Chart.register([
   CategoryScale,
@@ -32,6 +42,101 @@ Chart.register([
   Filler,
   Colors,
 ]);
+
+const getOrCreateLegendList = (chart: Chart, id: string) => {
+  const legendContainer = document.getElementById(id);
+  let listContainer = legendContainer?.querySelector('ul');
+
+  if (!listContainer) {
+    listContainer = document.createElement('ul');
+    listContainer.style.display = 'flex';
+    listContainer.style.flexDirection = 'row';
+    listContainer.style.margin = '0';
+    listContainer.style.padding = '0';
+
+    legendContainer?.appendChild(listContainer);
+  }
+
+  return listContainer;
+};
+
+const htmlLegendPlugin = {
+  id: 'htmlLegend',
+  afterUpdate(
+    chart: Chart,
+    args: {
+      mode:
+        | 'resize'
+        | 'reset'
+        | 'none'
+        | 'hide'
+        | 'show'
+        | 'default'
+        | 'active';
+    },
+    options: { containerID: string },
+  ) {
+    const ul = getOrCreateLegendList(chart, options.containerID);
+
+    // Remove old legend items
+    while (ul.firstChild) {
+      ul.firstChild.remove();
+    }
+
+    // Reuse the built-in legendItems generator
+    const items = chart.options.plugins?.legend?.labels?.generateLabels
+      ? chart.options.plugins.legend.labels.generateLabels(chart)
+      : [];
+
+    items.forEach((item) => {
+      const li = document.createElement('li');
+      li.style.alignItems = 'center';
+      li.style.cursor = 'pointer';
+      li.style.display = 'flex';
+      li.style.flexDirection = 'row';
+      li.style.marginLeft = '10px';
+
+      li.onclick = () => {
+        const { type } = chart.config as { type: string };
+        if (type === 'pie' || type === 'doughnut') {
+          // Pie and doughnut charts only have a single dataset and visibility is per item
+          chart.toggleDataVisibility(item.index || 0);
+        } else {
+          chart.setDatasetVisibility(
+            item.datasetIndex || 0,
+            !chart.isDatasetVisible(item.datasetIndex || 0),
+          );
+        }
+        chart.update();
+      };
+
+      // Color box
+      const boxSpan = document.createElement('span');
+      boxSpan.style.background = item.fillStyle as string;
+      boxSpan.style.borderColor = item.strokeStyle as string;
+      boxSpan.style.borderWidth = `${item.lineWidth}px`;
+      boxSpan.style.display = 'inline-block';
+      boxSpan.style.flexShrink = '0';
+      boxSpan.style.height = '20px';
+      boxSpan.style.marginRight = '10px';
+      boxSpan.style.width = '20px';
+
+      // Text
+      const textContainer = document.createElement('p');
+      textContainer.style.color = item.fontColor as string;
+      textContainer.style.margin = '0';
+      textContainer.style.padding = '0';
+      textContainer.style.textDecoration = item.hidden ? 'line-through' : '';
+
+      const text = document.createTextNode(item.text);
+      textContainer.appendChild(text);
+
+      li.appendChild(boxSpan);
+      li.appendChild(textContainer);
+      ul.appendChild(li);
+    });
+  },
+};
 
 function getLabel(searchParams: Partial<SearchParams>) {
   if (searchParams.period === 'c' && searchParams.from && searchParams.to) {
@@ -83,26 +188,40 @@ export default function Graph() {
       options: {
         responsive: true,
         plugins: {
+          htmlLegend: {
+            // ID of the container to put the legend in
+            containerID: 'legend-container',
+          },
           legend: {
-            position: 'top',
+            display: false,
           },
           tooltip: {
             callbacks: {
               label: (context) =>
-                `€ ${context.parsed} • ${Math.round((context.parsed / context.dataset.data.reduce((tot, n) => tot + n)) * 100)}%`,
+                `€ ${context.parsed} • ${Math.round(
+                  (context.parsed /
+                    context.dataset.data
+                      .filter((x) => typeof x === 'number')
+                      .reduce((tot, n) => tot + n)) *
+                    100,
+                )}%`,
             },
           },
         },
       },
+      plugins: [htmlLegendPlugin],
     });
   });
   return (
     <div classList={{ main__wrapper: true, [styles.wrapper]: true }}>
-      <div data-testid="total-for-period" class={styles.totalWrapper}>
-        <div class={styles.total}>{formatMoney(getFilteredDataTotal())}</div>
-        <div>{getLabel(searchParams)}</div>
+      <div id="legend-container" />
+      <div class={styles.graphAndTotal}>
+        <div data-testid="total-for-period" class={styles.totalWrapper}>
+          <div class={styles.total}>{formatMoney(getFilteredDataTotal())}</div>
+          <div>{getLabel(searchParams)}</div>
+        </div>
+        <canvas class={styles.canvas} ref={canvas} />
       </div>
-      <canvas class={styles.canvas} ref={canvas} />
     </div>
   );
 }
